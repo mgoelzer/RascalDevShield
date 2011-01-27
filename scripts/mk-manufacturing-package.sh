ab4a54030bf2adcef549d137953d4c3540f001cf
Mike Goelzer <mike@mwgaa.com>
2011-01-27 03:49:26 -0800

    Script was not packaging GTO (top silk)

--- scripts/mk-manufacturing-package.sh
+++ scripts/mk-manufacturing-package.sh
@@ -2,5 +2,5 @@
 
 TODAY=`date +%Y-%m-%d`
 pushd RascalDevShield
-zip -j ../rtm/RDS-Gerbers-$TODAY.zip *.DRD *.GBL *.GBO *.GBS *.GTL *.GTP *.GTP *.GTS
+zip -j ../rtm/RDS-Gerbers-$TODAY.zip *.DRD *.GTL *.GBL *.GTO *.GTP *.GBO *.GTS *.GBS 
 popd
